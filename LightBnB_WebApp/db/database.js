6b262d1a07ef2fdd75b608214683975ab8f2fbe6
Raymond Lin <raymond.lin.7690@gmail.com>
2023-06-15 16:20:51 -0700

    Refactoring getAllProperties with SQL

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -134,12 +134,57 @@ const getAllReservations = (guest_id, limit = 10) => {
 //   return Promise.resolve(limitedProperties);
 // };
 
-const getAllProperties = (options, limit = 10) => {
+/* const getAllProperties = (options, limit = 10) => {
 
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
       console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}; */
+
+const getAllProperties = function (options, limit = 10) {
+	
+	const queryParams = [];
+	let queryString = `
+	SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+	FROM properties
+	JOIN property_reviews ON properties.id = property_id
+	`;
+  
+	if (options.owner_id) {
+	  queryParams.push(options.owner_id);
+	  queryString += `AND owner_id = $${queryParams.length}`;
+	}
+
+	if (options.city) {
+		queryParams.push(`%${options.city}%`);
+		queryString += `WHERE city LIKE $${queryParams.length} `;
+	  }
+
+	if (options.minimum_price_per_night && options.maximum_price_per_night) {
+		queryParams.push((options.minimum_price_per_night * 100), (options.maximum_price_per_night * 100));
+		queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`
+	}
+  
+	queryString += `GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`
+  }
+  
+  queryParams.push(limit);
+
+  queryString += ` ORDER BY cost_per_night 
+  LIMIT $${queryParams.length};`
+
+	return pool.query(queryString, queryParams)
+    .then((result) => {
       return result.rows;
     })
     .catch((err) => {
