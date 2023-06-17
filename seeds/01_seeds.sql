2c3ab252f777d7e625714d3223ff2d24892c85a2
Raymond Lin <raymond.lin.7690@gmail.com>
2023-06-16 18:27:20 -0700

    Re-ordering sequence of inserts to match schema

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,19 +1,19 @@
-INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
-VALUES (1, '2018-09-11', '2018-09-26', 1, 1),
-(2,'2019-01-04', '2019-02-01', 2, 2),
-(3, '2021-10-01', '2021-10-14', 3, 3);
-
 INSERT INTO users (id, name, email, password)
 VALUES (1, 'Bastien', 'bastien@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (2, 'Ron', 'ron@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (3, 'Lee', 'lee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
-VALUES (1, 1, 1, 1, 3, 'message'),
-(2, 2, 2, 2, 4, 'message'),
-(3, 3, 3, 3, 5, 'message');
-
 INSERT INTO properties (id, owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES (1, 1, 'Speed Lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, TRUE),
 (2, 2, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, TRUE),
 (3, 3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', 44583, TRUE);
+
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
+VALUES (1, '2018-09-11', '2018-09-26', 1, 1),
+(2,'2019-01-04', '2019-02-01', 2, 2),
+(3, '2021-10-01', '2021-10-14', 3, 3);
+
+INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
+VALUES (1, 1, 1, 1, 3, 'message'),
+(2, 2, 2, 2, 4, 'message'),
+(3, 3, 3, 3, 5, 'message');
